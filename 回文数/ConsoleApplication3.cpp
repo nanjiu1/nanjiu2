2459587de36ccf363ab3e9a63908d9ce054e4033
nanjiu <zhengdewen@stu.scu.edu.cn>
2023-11-06 17:04:13 +0800

    commit 回文数

---
+++ 回文数/ConsoleApplication3.cpp
@@ -0,0 +1,42 @@
+﻿// ConsoleApplication3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+
+    printf("输入一个整数: ");
+    scanf_s("%d", &n);
+
+    originalInteger = n;
+
+    // 翻转
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversedInteger = reversedInteger * 10 + remainder;
+        n /= 10;
+    }
+
+    // 判断
+    if (originalInteger == reversedInteger)
+        printf("%d 是回文数。", originalInteger);
+    else
+        printf("%d 不是回文数。", originalInteger);
+
+    return 0;
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
